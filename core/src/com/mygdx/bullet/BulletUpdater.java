a71f294fbc59a5ffcb23718cea4125c9fdc22ace
Tom <2748887019@qq.com>
2023-11-24 14:18:19 +0800

    add bulletUpdater to update bullets

---
+++ core/src/com/mygdx/bullet/BulletUpdater.java
@@ -0,0 +1,38 @@
+package com.mygdx.bullet;
+
+import com.mygdx.character.Enemy;
+import com.mygdx.character.Hero;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+/**
+ * @author Hades
+ */
+public class BulletUpdater implements Runnable {
+  private final List<Bullet> bullets;
+  private final List<Hero> heroes;
+  private final List<Enemy> enemies;
+
+  public BulletUpdater(List<Bullet> bullets, List<Hero> heroes, List<Enemy> enemies) {
+    this.bullets = bullets;
+    this.heroes = heroes;
+    this.enemies = enemies;
+  }
+
+  @Override
+  public void run() {
+    List<Bullet> dead = new CopyOnWriteArrayList<>();
+    for (Bullet bullet : bullets) {
+      if (bullet.getSprite().getRotation() > 90 || bullet.getSprite().getRotation() < -90) {
+        bullet.update(heroes);
+      } else {
+        bullet.update(enemies);
+      }
+
+      if (bullet.isDead()) {
+        dead.add(bullet);
+      }
+    }
+    bullets.removeAll(dead);
+  }
+}
