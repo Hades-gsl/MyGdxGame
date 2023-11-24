1c76c434ae935daa9ccc98842aeb8642955f5ae7
Tom <2748887019@qq.com>
2023-11-24 14:19:09 +0800

    add Constants to store constants

---
+++ core/src/com/mygdx/constants/Constants.java
@@ -0,0 +1,27 @@
+package com.mygdx.constants;
+
+import com.badlogic.gdx.graphics.Color;
+
+/**
+ * @author Hades
+ */
+public class Constants {
+  public static final float CAMERA_WIDTH = 32 * 10 + 100;
+  public static final float CAMERA_HEIGHT = 32 * 10;
+  public static final float ROWS = 10;
+  public static final float COLS = 10;
+  public static final float CELL_SIZE = 32;
+  public static final int CHARACTER_COUNT = 6;
+  public static final int INIT_HERO_COUNT = 5;
+  public static final int INIT_ENEMY_COUNT = 5;
+  public static final float BULLET_SPEED = 2;
+  public static final float MAP_WIDTH = 32 * 10;
+  public static final float MAP_HEIGHT = 32 * 10;
+  public static final int HERO_HP = 100;
+  public static final int HERO_ATK = 10;
+  public static final int ENEMY_HP = 100;
+  public static final int ENEMY_ATK = 10;
+  public static final int INTERVAL_MILLI = 2000;
+  public static final int BORDER_WIDTH = 3;
+  public static final Color BACKGROUND_COLOR = Color.TEAL;
+}
