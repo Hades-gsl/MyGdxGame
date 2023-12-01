4b5a65f02dbaf6ab7c4dae33caf2d9dc0b1c7e51
Tom <2748887019@qq.com>
2023-12-01 19:55:04 +0800

    test: add gameScreenTest

---
+++ core/test/com/mygdx/game/GameScreenTest.java
@@ -0,0 +1,36 @@
+package com.mygdx.game;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.badlogic.gdx.Game;
+import com.mygdx.testRunner.TestRunner;
+import org.junit.jupiter.api.*;
+
+class GameScreenTest {
+  static TestRunner testRunner;
+  static Game game;
+  GameScreen gameScreen;
+
+  @BeforeAll
+  static void setUpAll() {
+    game = new MyGdxGame();
+    testRunner = new TestRunner(game);
+  }
+
+  @AfterAll
+  static void tearDownAll() {
+    testRunner.exit();
+  }
+
+  @BeforeEach
+  void setUp() {
+    gameScreen = new GameScreen((MyGdxGame) game, true);
+  }
+
+  @Test
+  void show() {
+    assertNotNull(gameScreen);
+    gameScreen.show();
+  }
+
+}
