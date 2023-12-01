5f8c657af659e8513552c666b87a7933e1838f17
Tom <2748887019@qq.com>
2023-12-01 19:54:46 +0800

    test: add mock gl type

--- core/test/com/mygdx/testRunner/TestRunner.java
+++ core/test/com/mygdx/testRunner/TestRunner.java
@@ -7,17 +7,20 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.headless.HeadlessApplication;
 import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.GL30;
 
 public class TestRunner {
-    public TestRunner(ApplicationListener applicationListener){
-        HeadlessApplicationConfiguration config = new HeadlessApplicationConfiguration();
-        config.updatesPerSecond = 60;
-        new HeadlessApplication(applicationListener, config);
+  public TestRunner(ApplicationListener applicationListener) {
+    Gdx.gl = mock(GL20.class);
+    Gdx.gl20 = mock(GL20.class);
+    Gdx.gl30 = mock(GL30.class);
 
-        Gdx.gl = mock(GL20.class);
-    }
+    HeadlessApplicationConfiguration config = new HeadlessApplicationConfiguration();
+    config.updatesPerSecond = 60;
+    new HeadlessApplication(applicationListener, config);
+  }
 
-    public void exit(){
-        Gdx.app.exit();
-    }
+  public void exit() {
+    Gdx.app.exit();
+  }
 }
