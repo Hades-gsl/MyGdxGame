23f288e5301ee53306e4d2468f26a76981f62622
Tom <2748887019@qq.com>
2023-12-21 23:48:20 +0800

    style: add author

--- desktop/src/com/mygdx/game/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -4,13 +4,17 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.mygdx.config.Config;
 
-// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+/**
+ * @author Hades
+ */
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM
+// argument
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setForegroundFPS(60);
-		config.setTitle("My GDX Game");
-		config.setWindowedMode((int) Config.CAMERA_WIDTH * 3, (int) Config.CAMERA_HEIGHT * 3);
-		new Lwjgl3Application(new MyGdxGame(), config);
-	}
+  public static void main(String[] arg) {
+    Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+    config.setForegroundFPS(60);
+    config.setTitle("My GDX Game");
+    config.setWindowedMode((int) Config.CAMERA_WIDTH * 2, (int) Config.CAMERA_HEIGHT * 2);
+    new Lwjgl3Application(new MyGdxGame(), config);
+  }
 }
